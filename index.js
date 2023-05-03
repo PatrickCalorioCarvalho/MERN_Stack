a2b245d91d51c0f66105cfdb727b08e8ea812372
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2023-05-03 00:05:39 -0300

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const userRoute = require('./src/routes/user.route');
+
+const port = 3000
+
+app.use(express.json());
+app.use('/api/user',userRoute);
+
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+});
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const create = (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json(user);
+};
+module.exports = {create};
