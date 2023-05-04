6b4c7d2252e94fefa75b09596709e4928cc60166
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2023-05-03 22:27:14 -0300

    find and update users

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,8 @@
-const route = require('express').Router();
-const userController = require('../controllers/user.controller');
+const route = require("express").Router();
+const userController = require("../controllers/user.controller");
 
-
-route.post("/",userController.create);
-
+route.get("/", userController.findAll);
+route.get("/:id", userController.findById);
+route.post("/", userController.create);
+route.patch("/:id", userController.update);
 module.exports = route;
